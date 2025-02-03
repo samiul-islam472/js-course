2bb5f021c168ce75a03c52b35fb65c9d8a2f42c0
samiul-islam472 <mdsamiulislam4678@gmail.com>
2025-02-03 10:18:02 +0600

    Add 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // Use/Treat all JS code as newer version
+
+// alert(3 + 3) // We are using node.js, not browser
+
+let name = "Samiul";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// Datatypes in JS
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Samiul"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
