2527ce6bf555fc0243aa5e710c3c90315cd617a7
samiul-islam472 <mdsamiulislam4678@gmail.com>
2025-02-03 12:36:33 +0600

    Add further work

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score); // type conversion
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);  // type conversion
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false;
 // "" => false
@@ -22,5 +22,36 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber); // type conversion
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  Note: To convert a variable's datatype first you need to declare the variable and assign a value to it then you need to declare a second variable and assign the first variable as its value followed by the desired datatype. 
+
+// ******************** OPERATIONS ********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+ 
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+let str1 = "Hello";
+let str2 = " Samiul";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(1 + 1);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
